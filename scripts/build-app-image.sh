de381a9ba68a740ba8d17eca89834e3d8e702b5a
shellclear <12013211+shellclear@users.noreply.github.com>
2022-01-29 17:34:57 +0100

    new build-app-image.sh

--- scripts/build-app-image.sh
+++ scripts/build-app-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 rm -fr s2i/build-app-image/*
-rsync -vahziAX --delete app/hello/ s2i/build-image/test/test-app/
-s2i build s2i/build-image/test/test-app s2i-python:39 python-hello:1.0 --as-dockerfile s2i/build-app-image/Dockerfile
+rsync -vahziAX --delete app/hello/ s2i/build-image/python-hello
+s2i build s2i/build-image/python-hello s2i-python:39 python-hello:1.0 --as-dockerfile s2i/build-app-image/Dockerfile
 podman build --format docker -t python-hello:1.0 s2i/build-app-image
