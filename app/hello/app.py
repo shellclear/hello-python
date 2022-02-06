0116eff26d5a727351b6f9b5326d84e84f916f78
Paulo Pacifico <12013211+shellclear@users.noreply.github.com>
2022-02-06 17:38:51 +0100

    change app to support APP_VERSION var

--- app/hello/app.py
+++ app/hello/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 from flask import Flask,render_template
 import socket
+import os
 
 hostname = socket.gethostname()
 ipaddress = socket.gethostbyname(hostname)
+appver = os.environ['APP_VERSION']
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', hostname=hostname, ipaddress=ipaddress)
+    return render_template('index.html', hostname=hostname, ipaddress=ipaddress, appver=appver)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
